ad20656f8c56664bfd9a7f71107f52fc2c4b3baf
c-xulj02 <c-xulj02@vanke.com>
2022-07-18 16:06:15 +0800

    xu

---
+++ precache-manifest.31de6399c6c1b27fb895f2ae6f11b2b4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b439aa305a49d78895db765d2abab58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3efb5f874875bef86426",
+    "url": "/xulongji/static/css/2.0ac1cc9d.chunk.css"
+  },
+  {
+    "revision": "ff4f07cd4be61a707f5f",
+    "url": "/xulongji/static/css/main.2ba715d5.chunk.css"
+  },
+  {
+    "revision": "3efb5f874875bef86426",
+    "url": "/xulongji/static/js/2.607cddc6.chunk.js"
+  },
+  {
+    "revision": "5f649f6dab50d3cb9564f3dcfa2f32a9",
+    "url": "/xulongji/static/js/2.607cddc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4f07cd4be61a707f5f",
+    "url": "/xulongji/static/js/main.38ced4bb.chunk.js"
+  },
+  {
+    "revision": "23da56ba2b485594d1ca",
+    "url": "/xulongji/static/js/runtime-main.96de2f7a.js"
+  },
+  {
+    "revision": "07538e56c341315ab5e711cbf5d4a3b6",
+    "url": "/xulongji/static/media/b1.07538e56.jpg"
+  },
+  {
+    "revision": "409ee65fdd039fcce394fc94e0dd44bc",
+    "url": "/xulongji/static/media/girl.409ee65f.gif"
+  },
+  {
+    "revision": "7d4c4dafea90439b8017b6136442647a",
+    "url": "/xulongji/static/media/iconfont.7d4c4daf.eot"
+  },
+  {
+    "revision": "983341df1b4fc42705c815a9a44e6175",
+    "url": "/xulongji/static/media/iconfont.983341df.ttf"
+  },
+  {
+    "revision": "9e47d2e81360194fc675573a04113da4",
+    "url": "/xulongji/static/media/iconfont.9e47d2e8.woff"
+  },
+  {
+    "revision": "ffd857227e529ea676cad32dce392ee8",
+    "url": "/xulongji/static/media/iconfont.ffd85722.svg"
+  },
+  {
+    "revision": "6524ca559ebf36abb0fb95ae03195449",
+    "url": "/xulongji/static/media/logo.6524ca55.jpg"
+  }
+]);
